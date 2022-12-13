c0ca20ced79349f8084b6d31450fc06171e45508
mamin89 <mina_amin89@yahoo.com>
2022-12-13 16:10:23 -0500

    hw

---
+++ src/data.js
@@ -0,0 +1,64 @@
+export default [
+    {
+      name: "Kimg, James",
+      employeeId: 1, 
+      title: "President & CEO",
+      img:
+        "https://cdn1.vectorstock.com/i/1000x1000/30/15/young-happy-businessman-cartoon-flat-vector-18203015.jpg",
+      office:"contact via phone : 212-444-2222",
+      email: "contact via e-mail: fking@fakemailmeow.com",
+      SMS: 123456789
+    },
+    {
+      name: "Taylor, Julia",
+      employeeId: 2,
+      title: "VP of Marketing",
+      img:
+        "https://i.etsystatic.com/10919371/r/il/94ee18/1630459851/il_794xN.1630459851_j72k.jpg",
+        office:"Contact via phone: 212-444-2522",
+      email: "contact via e-mail mail:jTaylor@fakemail.com",
+      SMS: 123456789
+    },
+    {
+      name: "Leww, Eugene",
+      employeeId: 3,
+      title: "CFO",
+      img:
+        "https://c8.alamy.com/comp/RP23P7/business-man-cartoon-character-handsome-businessman-holding-documents-vector-illustration-on-white-background-RP23P7.jpg",
+        office:"contact via phone: 212-444-2422",
+      email: "contact via e-mail: lee.e@fakemail.com",
+      SMS: 123456789
+    },
+    {
+      name: "Williams, John",
+      employeeId: 4,
+      title: "VP of Engineering",
+      img:
+        "https://st.depositphotos.com/3246207/4385/i/600/depositphotos_43852589-stock-photo-cartoon-businessman-character.jpg",
+        office:"contact via phone: Call office: 212-444-2722",
+      email: "contact via e-mail: will.j@fakemail.com",
+      SMS: 123456789
+    },
+    {
+      name: "Moore, Ray",
+      employeeId: 5,
+      title: "VP of Sales",
+      img:
+        "https://c8.alamy.com/comp/R4AEK2/business-man-cartoon-character-in-formal-suit-handsome-businessman-holds-modern-laptop-manager-banker-vector-illustration-R4AEK2.jpg",
+
+      mobile: "contact via phone : 111-666-3333",
+      email: " contact via e-mail: Rmoore@fakemail.com",
+      SMS: 123456789
+    },
+    {
+      name: "Jones, Paul",
+      employeeId: 6, 
+      title: "OA Manager",
+      img:
+        "https://cdn2.vectorstock.com/i/1000x1000/12/71/smiling-business-man-cartoon-vector-11141271.jpg",
+        office:"Call office: 212-444-2202",
+      mobile: "contact via phone: 111-222-3333",
+      email: "contavt via e-mail: pjones@fakemail.com",
+      SMS: 123456789
+    }
+  ];
